d73a883820e2bbd9b515098e3c3b930d76e8c67f
Ibrahim <i.alamin5@gmail.com>
2016-06-30 15:40:47 -0400

    update settings.py database

--- webapp/settings.py
+++ webapp/settings.py
@@ -147,3 +147,9 @@ STATICFILES_DIRS = (
 # https://warehouse.python.org/project/whitenoise/
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
